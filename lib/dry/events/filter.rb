9cdc568e9fbce54ddb25def8888ebe0ee0b2dad4
Nikita Shilnikov <fg@flashgordon.ru>
2019-02-20 00:48:37 +0300

    Add docs [skip ci]

--- lib/dry/events/filter.rb
+++ lib/dry/events/filter.rb
@@ -2,19 +2,40 @@ require 'set'
 
 module Dry
   module Events
+    # Event filter
+    #
+    # A filter cherry-picks probes payload of events.
+    # Events not matching the predicates don't fire callbacks.
+    #
+    # @api private
     class Filter
       NO_MATCH = Object.new.freeze
 
+      # @!attribute [r] events
+      #   @return [Array] A list of lambdas checking payloads
       attr_reader :checks
 
+      # Create a new filter
+      #
+      # @param [Hash] filter Source filter
+      #
+      # @api private
       def initialize(filter)
         @checks = build_checks(filter)
       end
 
+      # Test event payload against the checks
+      #
+      # @param [Hash] payload Event payload
+      #
+      # @api private
       def call(payload = EMPTY_HASH)
         checks.all? { |check| check.(payload) }
       end
 
+      # Recursively build checks
+      #
+      # @api private
       def build_checks(filter, checks = EMPTY_ARRAY, keys = EMPTY_ARRAY)
         if filter.is_a?(Hash)
           filter.reduce(checks) do |cs, (key, value)|
@@ -25,6 +46,7 @@ module Dry
         end
       end
 
+      # @api private
       def compare(path, predicate, payload)
         value = path.reduce(payload) do |acc, key|
           if acc.is_a?(Hash) && acc.key?(key)
@@ -37,6 +59,7 @@ module Dry
         predicate.(value)
       end
 
+      # @api private
       def predicate(value)
         case value
         when Proc then value
