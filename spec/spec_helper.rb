01a8ed53c51c91a1db6df307abb0addd3baf1b1f
Adam Lassek <adam@doubleprime.net>
2025-09-04 23:31:32 -0500

    Migrate project to Hanakai's repo-sync automation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,10 +14,6 @@ Dir[SPEC_ROOT.join("shared/**/*.rb")].each(&method(:require))
 Dir[SPEC_ROOT.join("support/**/*.rb")].each(&method(:require))
 
 RSpec.configure do |config|
-  config.warnings = true
-  config.disable_monkey_patching!
-  config.filter_run_when_matching :focus
-
   config.after(:example) do
     Dry::Events::Publisher.instance_variable_set(:@__registry__, Concurrent::Map.new)
   end
